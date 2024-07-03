bcd8c3ab13b11dd96e83eb85fafef2bf5647e323
Kaio Delphino <github@kaiodelphino.com>
2024-07-03 18:09:41 -0300

    feat: build tree when fetching locations

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -38,15 +38,74 @@ class Controller {
     Company company, {
     required ErrorCallback onError,
   }) async {
-    if (!_isLoading.value) _isLoading.value = true;
+    _isLoading.value = true;
 
     try {
-      locations = await repo.fetchLocations(company);
+      final locations = await repo.fetchLocations(company);
+      final assets = await fetchAssets(company, onError: onError);
+      company.children = _buildTree(company, locations, assets);
     } catch (e) {
       if (kDebugMode) debugPrint(e.toString());
       onError(e);
     }
 
     _isLoading.value = false;
+  }
+
+  Future<Set<Asset>> fetchAssets(
+    Company company, {
+    required ErrorCallback onError,
+  }) async {
+    _isLoading.value = true;
+
+    try {
+      final assets = await repo.fetchAssets(company);
+      return assets;
+    } catch (e) {
+      if (kDebugMode) debugPrint(e.toString());
+      onError(e);
+    }
+
+    _isLoading.value = false;
+    return {};
+  }
+
+  Set<TreeNode> _buildTree(
+    Company company,
+    Set<Location> location,
+    Set<Asset> assets,
+  ) {
+    // create a map to hold all nodes by their id
+    Map<String, TreeNode> nodeMap = {};
+
+    // add nodes to nodeMap
+    for (var node in location) {
+      nodeMap[node.id] = node;
+    }
+    for (var node in assets) {
+      nodeMap[node.id] = node;
+    }
+
+    // create new tree
+    final tree = <TreeNode>{};
+    // popule tree by assigning nodes to their parents
+    nodeMap.forEach((id, node) {
+      // populate nodes that are on tree root
+      if (node.parentNodeId == null) {
+        tree.add(node);
+        return;
+      }
+
+      // find parent node
+      final parentNode = nodeMap[node.parentNodeId];
+      // throw error if parent node could not be found
+      if (parentNode == null) {
+        throw Exception('Couldn\'t find the node: ${node.parentNodeId}');
+      }
+      // add node in parent
+      return parentNode.add(node);
+    });
+
+    return tree;
   }
 }
